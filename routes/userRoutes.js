495d1472a15d44ce2d1c1aebf27344bd6a2b74c0
Rami_Nawahda <rami.nawahda@aerdata.com>
2024-05-05 22:09:45 +0300

    Add authorization middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const {
     loginController,
     registerController,
+    authController,
 } = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 //router
 const router = express.Router();
@@ -13,4 +15,8 @@ router.post("/login", loginController);
 
 //REGISTER || POST
 router.post("/register", registerController);
+
+//AUTHORIZATION || POST
+router.post("/getUserData", authMiddleware, authController);
+
 module.exports = router;
